469a0209341c3fe713289e08330a0e1e00078cfa
WenT1223 <frictobias@gmail.com>
2026-02-01 13:12:47 +0100

    Added Path(), made while loop

--- python/Level1/tracker.py
+++ python/Level1/tracker.py
@@ -1,27 +1,36 @@
+from pathlib import Path
+BASE_DIR = Path(__file__).parent
+TASKS_FILE = BASE_DIR / "tasks.txt"
+
+
 def menu():
-    option = input("1. Add task\n2. Show tasks\n3. Exit\n\nChoose: ")
-    if option == "1":
-        add_task()
-    elif option == "2":
-        show_tasks()
-    elif option == "3":
-        quit()
-    else:
-        print("\nWrong input!\nCorrect form: 1 / 2 / 3\n")
-        menu()
+    while True:
+        option = input("1. Add task\n"
+                       "2. Show tasks\n"
+                       "3. Exit\n\n"
+                       "Choose: ")
+        if option == "1":
+            add_task()
+        elif option == "2":
+            show_tasks()
+        elif option == "3":
+            print("EXITTING\n--------------------------------")
+            break
+        else:
+            print("\nWrong input!\nCorrect form: 1 / 2 / 3\n")
 
 
 def add_task():
     task = input("Task: ")
-    with open("C:/Users/frict/Documents/VSCODE/it-skill-tree/python/Level1/tasks.txt", "a") as f:
+    with open(TASKS_FILE, "a") as f:
         f.write(task+"\n")
-        menu()
+        return
 
 
 def show_tasks():
-    with open("C:/Users/frict/Documents/VSCODE/it-skill-tree/python/Level1/tasks.txt", "r") as f:
+    with open(TASKS_FILE, "r") as f:
         print(f.read())
-        menu()
+        return
 
 
 menu()
