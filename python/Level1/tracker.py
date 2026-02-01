0776c08d737fe9cdb664451e28df468a136eb8c9
WenT1223 <frictobias@gmail.com>
2026-02-01 12:44:55 +0100

    Added function menu

--- python/Level1/tracker.py
+++ python/Level1/tracker.py
@@ -1,6 +1,27 @@
-task = input("Task: ")
-tasks = open("python/Level1/tasks.txt", "a")
-tasks.write(task+"\n")
-tasks = open("python/Level1/tasks.txt", "r")
+def menu():
+    option = input("1. Add task\n2. Show tasks\n3. Exit\n\nChoose: ")
+    if option == "1":
+        add_task()
+    elif option == "2":
+        show_tasks()
+    elif option == "3":
+        quit()
+    else:
+        print("\nWrong input!\nCorrect form: 1 / 2 / 3\n")
+        menu()
 
-print(tasks.read())
+
+def add_task():
+    task = input("Task: ")
+    with open("C:/Users/frict/Documents/VSCODE/it-skill-tree/python/Level1/tasks.txt", "a") as f:
+        f.write(task+"\n")
+        menu()
+
+
+def show_tasks():
+    with open("C:/Users/frict/Documents/VSCODE/it-skill-tree/python/Level1/tasks.txt", "r") as f:
+        print(f.read())
+        menu()
+
+
+menu()
